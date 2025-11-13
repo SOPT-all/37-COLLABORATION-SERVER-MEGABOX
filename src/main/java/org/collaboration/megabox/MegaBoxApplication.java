8566afac4606ae60123a9d8fd59fbd074f1bdb83
kyoooooong <haileyre000b@naver.com>
2025-11-14 00:25:01 +0900

    initial commit

---
+++ src/main/java/org/collaboration/megabox/MegaBoxApplication.java
@@ -0,0 +1,13 @@
+package org.collaboration.megabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaBoxApplication.class, args);
+	}
+
+}
