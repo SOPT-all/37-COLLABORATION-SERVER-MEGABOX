1b2c1b4cd47b19d8cc96d982465d500434d7dd1d
kyoooooong <haileyre000b@naver.com>
2025-11-14 01:02:20 +0900

    feat: SwaggerConfig 구현

---
+++ src/main/java/org/collaboration/megabox/global/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package org.collaboration.megabox.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "37-COLLABORATION-SERVER-MEGABOX Server API",
+        version = "1.0",
+        description = "DIVE SOPT 합동 세미나 모바일 웹 2조 메가박스 서버 API SWAGGER 입니다."
+    )
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server localServer = new Server()
+            .url("http://localhost:8080")
+            .description("Local Server");
+
+//        Server prodServer = new Server()
+//            .url("")
+//            .description("Prod Server");
+
+        return new OpenAPI().servers(List.of(localServer));
+    }
+}
