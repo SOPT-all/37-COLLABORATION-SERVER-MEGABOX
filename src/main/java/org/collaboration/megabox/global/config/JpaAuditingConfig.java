107816a8ec1159754ec30c588a9cf0b5a7eaee61
kyoooooong <haileyre000b@naver.com>
2025-11-14 01:02:56 +0900

    feat: JpaAuditingConfig 구현

---
+++ src/main/java/org/collaboration/megabox/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package org.collaboration.megabox.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
