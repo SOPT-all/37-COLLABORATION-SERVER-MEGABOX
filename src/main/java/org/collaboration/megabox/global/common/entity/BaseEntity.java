7ff171c22acf9a88d7b8872923a4ed5e6d19c751
kyoooooong <haileyre000b@naver.com>
2025-11-14 17:38:55 +0900

    feat: BaseEntity 구현

---
+++ src/main/java/org/collaboration/megabox/global/common/entity/BaseEntity.java
@@ -0,0 +1,31 @@
+package org.collaboration.megabox.global.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Comment("등록일시")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    @Comment("수정일시")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    @Comment("삭제일시")
+    private LocalDateTime deletedAt;
+}
