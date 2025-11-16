45b82d74e60d448026dcac42ce2037caa58f26cd
hyomee2 <s5763305@naver.com>
2025-11-17 00:38:50 +0900

    chore: ReviewRepositoryCustomImpl에 @Repository 명시

--- src/main/java/org/collaboration/megabox/domain/repository/ReviewRepositoryCustomImpl.java
+++ src/main/java/org/collaboration/megabox/domain/repository/ReviewRepositoryCustomImpl.java
@@ -1,4 +1,7 @@
 package org.collaboration.megabox.domain.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ReviewRepositoryCustomImpl implements ReviewRepositoryCustom {
 }
