ba1769939d4ba16badc24026e680420f60445185
민경 <149250433+kyoooooong@users.noreply.github.com>
2025-11-16 16:19:39 +0900

    feat: 영화 상세 조회 api 작성 (#20)
    * feat: Movie 엔티티 rank 컬럼 수정
    
    * feat: Movie repository 생성
    
    * feat: Movie 목록 조회 api 작성
    
    * feat: Movie 목록 조회 api swagger 작성
    
    * feat: Movie 상세 조회 api 작성

--- src/main/java/org/collaboration/megabox/domain/controller/MovieController.java
+++ src/main/java/org/collaboration/megabox/domain/controller/MovieController.java
@@ -2,11 +2,13 @@ package org.collaboration.megabox.domain.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.collaboration.megabox.domain.dto.response.MovieDetailResponse;
 import org.collaboration.megabox.domain.dto.response.MovieListResponse;
 import org.collaboration.megabox.domain.service.MovieService;
 import org.collaboration.megabox.global.common.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,20 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<ApiResponse<MovieListResponse>> getList() {
         MovieListResponse response = movieService.getList();
+
+        return ApiResponse.success(response);
+    }
+
+    @Operation(
+        summary = "영화 상세 조회",
+        description = """
+            선택한 영화의 상세 정보를 조회하는 API입니다.
+            - Path Variable로 movieId(영화 ID)를 전달합니다.
+            """
+    )
+    @GetMapping("/{movieId}")
+    public ResponseEntity<ApiResponse<MovieDetailResponse>> getDetail(@PathVariable Long movieId) {
+        MovieDetailResponse response = movieService.getDetail(movieId);
 
         return ApiResponse.success(response);
     }
