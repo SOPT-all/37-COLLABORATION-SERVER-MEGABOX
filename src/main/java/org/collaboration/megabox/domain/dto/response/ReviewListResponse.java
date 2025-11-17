01e60c60ef6126786b1606e9e1bb98429b3a9286
hyomee2 <s5763305@naver.com>
2025-11-17 14:44:04 +0900

    refactor: ReviewResponse, ReviewListResponse 구조 리팩토링 - ReviewListResponse 내에 ReviewResonse를 중첩 레코드로 선언했습니다. - 리뷰 리스트 변환 로직을 서비스 -> DTO의 정적 팩토리 메서드로 책임을 위임했습니다. (응집도, 가독성)

--- src/main/java/org/collaboration/megabox/domain/dto/response/ReviewListResponse.java
+++ src/main/java/org/collaboration/megabox/domain/dto/response/ReviewListResponse.java
@@ -1,13 +1,40 @@
 package org.collaboration.megabox.domain.dto.response;
 
+import org.collaboration.megabox.domain.entity.Review;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record ReviewListResponse(
-        Long movieId,
         Integer reviewCount,
         List<ReviewResponse> reviews
 ) {
-    public static ReviewListResponse of(Long movieId, List<ReviewResponse> reviews) {
-        return new ReviewListResponse(movieId, reviews.size(), reviews);
+    public static ReviewListResponse of(int reviewCount, List<ReviewResponse> reviews) {
+        return new ReviewListResponse(reviewCount, reviews);
+    }
+
+    public record ReviewResponse(
+            Long reviewId,
+            Integer rating,
+            Long memberId,
+            String nickname,
+            String content,
+            LocalDateTime createdAt
+    ) {
+        public static ReviewResponse from(Review review) {
+            return new ReviewResponse(
+                    review.getReviewId(),
+                    review.getRating(),
+                    review.getMember().getMemberId(),
+                    review.getMember().getNickname(),
+                    review.getContent(),
+                    review.getCreatedAt()
+            );
+        }
+
+        public static List<ReviewResponse> from(List<Review> reviews) {
+            return reviews.stream()
+                    .map(ReviewResponse::from)
+                    .toList();
+        }
     }
 }
