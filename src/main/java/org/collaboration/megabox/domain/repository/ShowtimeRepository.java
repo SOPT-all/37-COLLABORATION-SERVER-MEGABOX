bdcb725415a067a8cf777c8fc1781549a4c792c4
hyomee2 <s5763305@naver.com>
2025-11-19 16:42:32 +0900

    refactor: showtime 조회 fetch join으로 변경

--- src/main/java/org/collaboration/megabox/domain/repository/ShowtimeRepository.java
+++ src/main/java/org/collaboration/megabox/domain/repository/ShowtimeRepository.java
@@ -2,6 +2,19 @@ package org.collaboration.megabox.domain.repository;
 
 import org.collaboration.megabox.domain.entity.Showtime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ShowtimeRepository extends JpaRepository<Showtime, Long> {
+
+    @Query("""
+        SELECT s
+        FROM Showtime s
+            JOIN FETCH s.movie
+            JOIN FETCH s.theater t
+            JOIN FETCH t.cinema
+        WHERE s.showtimeId = :showtimeId
+    """)
+    Optional<Showtime> findShowtimeInfoById(Long showtimeId);
 }
