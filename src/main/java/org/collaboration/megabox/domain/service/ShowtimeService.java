9e98fad9bda6c8305445389855a5c3f1ccbc589b
hyomee2 <s5763305@naver.com>
2025-11-19 16:42:14 +0900

    chore: @Transactional(readOnly = true) 추가

--- src/main/java/org/collaboration/megabox/domain/service/ShowtimeService.java
+++ src/main/java/org/collaboration/megabox/domain/service/ShowtimeService.java
@@ -7,15 +7,17 @@ import org.collaboration.megabox.domain.repository.ShowtimeRepository;
 import org.collaboration.megabox.global.exception.CustomException;
 import org.collaboration.megabox.global.exception.ErrorCode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ShowtimeService {
 
     private final ShowtimeRepository showtimeRepository;
 
     public ShowtimeBeforeReservationResponse getShowtimeBeforeReservation(Long showtimeId) {
-        Showtime showtime = showtimeRepository.findById(showtimeId)
+        Showtime showtime = showtimeRepository.findShowtimeInfoById(showtimeId)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_SHOWTIME));
         return ShowtimeBeforeReservationResponse.from(showtime);
     }
